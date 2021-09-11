c9c9c8e76f239042db7fb6812a4bb581e85cabb9
Alessandro Carra <22fimdetarde@gmail.com>
2021-09-11 01:25:59 -0300

    Static charts

---
+++ frontend/src/components/DonutChart/index.tsx
@@ -0,0 +1,31 @@
+import Chart from 'react-apexcharts';
+
+const DonutChart = () => {
+    const mockData = {
+        series: [477138, 499928, 444867, 220426, 473088],
+        labels: ['Anakin', 'Han-Solo', 'Chewbacca', 'Luke-Skywalker', 'Padmé']
+    }
+    
+    const options = {
+        legend: {
+            show: true
+        }
+    }
+    
+
+
+    return (
+
+        <Chart 
+            options={{ ...options, labels: mockData.labels}}
+            series={mockData.series}
+            type="donut"
+            height="240"
+
+            />
+     
+
+    );
+}
+
+export default DonutChart;
